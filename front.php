df267156b0efb1d93b7f89c4fd0eff851f522b07
Franklin Shih <franklin.shih@gmail.com>
2014-09-09 21:28:03 -0700

    created PHP shell

--- front.php
+++ front.php
@@ -1,7 +1,10 @@
 <html>
 	<?php
-		for ($i = 0; $i < 10; $i++) {
-			print("$i");
+		$injuryListing = "http://www.fantasyfootballnerd.com/service/injuries/xml/2iacgnksv3vr/1/";
+		if($response_xml_data){
+			echo "read";
 		}
+		$data = simplexml_load_string($response_xml_data);
+		echo "<pre>"; print_r($data); exit; 
 	?>
 </html>
