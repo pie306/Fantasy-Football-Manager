ac0668d6aa279b739b88522db16f48f0a2adb5b7
Franklin Shih <franklin.shih@gmail.com>
2014-09-10 15:36:32 -0700

    scraping

--- front.php
+++ front.php
@@ -42,14 +42,13 @@
 		$titles = $dom->getElementsByTagName("a");
 		$count = 0;
 		foreach ($titles as $title) {
-			/*if ($title->getAttribute("fp-player-name") != "") {
+			if ($title->getAttribute("fp-player-name") !== "" || $title->getAttribute("fp-player-name") !== NULL) {
 				count++;
 				if ($title->getAttribute("fp-player-name") == "A.J. Green") {
 					break;
 				}
-			}*/
-			echo($title->getAttribute("fp-player-name"));
+			}
 		}
-		//echo($count);
+		echo($count);
 	?>
 </html>
