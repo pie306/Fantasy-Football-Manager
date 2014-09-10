3a10c49ccbf1aa4f4e0515879ccde6dcfd118a80
Franklin Shih <franklin.shih@gmail.com>
2014-09-09 21:50:10 -0700

    injury

--- front.php
+++ front.php
@@ -2,10 +2,13 @@
 	<?php
 		$injury_URL = "http://www.fantasyfootballnerd.com/service/injuries/xml/2iacgnksv3vr/1/";
 		$injury_data = file_get_contents($injury_URL);
-		if($injury_data){
-			echo "read";
-		}
 		$data = simplexml_load_string($injury_data);
-		echo "<pre>"; print_r($data); exit; 
+		//echo "<pre>"; print_r($data); exit; 
+		echo "<ul>";
+		foreach ($data->Team as $currentTeam) :
+			$teamName = $currentTeam->code;
+			echo "<li>$teamName</li>";
+		endforeach;
+		echo "</ul>";
 	?>
 </html>
