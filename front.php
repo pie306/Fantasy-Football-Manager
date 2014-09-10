5712841a03baa6a19e9ec04d7a42e5d62efb14b4
Franklin Shih <franklin.shih@gmail.com>
2014-09-09 21:33:28 -0700

    injury

--- front.php
+++ front.php
@@ -1,10 +1,12 @@
 <html>
 	<?php
-		$injuryListing = "http://www.fantasyfootballnerd.com/service/injuries/xml/2iacgnksv3vr/1/";
-		if($response_xml_data){
+		/*$injury_URL = "http://www.fantasyfootballnerd.com/service/injuries/xml/2iacgnksv3vr/1/";
+		$injury_data = file_get_contents($injury_URL);
+		if($injury_data){
 			echo "read";
 		}
-		$data = simplexml_load_string($response_xml_data);
-		echo "<pre>"; print_r($data); exit; 
+		$data = simplexml_load_string($injury_data);
+		echo "<pre>"; print_r($data); exit; */
 	?>
+	5
 </html>
