06aa0e8dafe558d412a76a590bf49e503e132924
Franklin Shih <franklin.shih@gmail.com>
2014-09-12 18:54:13 -0700

    server

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php include "base.php"; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">  
+<!DOCTYPE html>
+<html>  
 	<head>  
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
 		<title>Fantasy Football Manager</title>
@@ -9,6 +8,7 @@
 	<body>  
 		<div id="main">
 			<?php
+				include "base.php";
 				if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
 			?>
 					<h1>Login</h1>
