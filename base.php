6298ac1030ed533ef14bc5ba87ee1612378029a9
Franklin Shih <franklin.shih@gmail.com>
2014-09-13 00:23:55 -0700

    server

--- base.php
+++ base.php
@@ -6,8 +6,8 @@
 	$dbuser = "b73bf768820bf5"; // the username that you created, or were given, to access your database
 	$dbpass = "ab827e68"; // the password that you created, or were given, to access your database
 	 
-	mysql_connect($dbhost, $dbuser, $dbpass);// or die("MySQL Error: " . mysql_error());
-	mysql_select_db($dbname);// or die("MySQL Error: " . mysql_error());
+	mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);// or die("MySQL Error: " . mysql_error());
+	//mysql_select_db($dbname);// or die("MySQL Error: " . mysql_error());
 	//DATABASE_URL:         mysql://b73bf768820bf5:ab827e68@us-cdbr-iron-east-01.cleardb.net/heroku_3459d78c3442ad9?reconnect=true
 	
     /*$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
