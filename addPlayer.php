281a7c17181791302d79005ae5a6ea5f23ba6a1c
Franklin Shih <franklin.shih@gmail.com>
2014-09-13 22:49:01 -0700

    server

--- addPlayer.php
+++ addPlayer.php
@@ -11,13 +11,13 @@
 			if ($i == NULL) {
 				break;
 			}
-		}/*
+		}
 		if ($i != 17) {
 			$playerNum = "Player" . $i;
 			mysqli_query($success, "SET SQL_SAFE_UPDATES=0");
 			mysqli_query($success, "UPDATE players SET $playerNum = '$playerName' WHERE Username = '$user'");
-			mysqli_query($success, "INSERT INTO positions (Username, Player, Position) VALUES('$usern', '$playerName', '$position')");
-		}*/
+			//mysqli_query($success, "INSERT INTO positions (Username, Player, Position) VALUES('$usern', '$playerName', '$position')");
+		}
 		header("Location: index.php");
 		
 		/*$weekNumber = 2;
